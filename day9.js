3818da42fa782067b559684a0c78d34b6bc24603
MJACoder <72562331+MJACodeR@users.noreply.github.com>
2023-11-11 08:05:24 +0530

    change

--- day9.js
+++ day9.js
@@ -1,20 +1,35 @@
 //create an object using a constructor function in JavaScript?
-function Person(name,age){
-    this.age = age;
-    this.name = name;
+function Person(name, age) {
+  this.age = age;
+  this.name = name;
 }
-let person = new Person("abhi",32)
-console.log(person.age)
+let person = new Person("abhi", 32);
+console.log(person.age);
 //How do you create an object using prototypal inheritance in JavaScript?
 let emp = {
-    name : "amardeep",
-    salary: 50000,
-    designation:"senior developer",
-    details:function(){
-        return `Name is ${this.name}, Salary is ${this.salary}$ and Designation is ${this.designation}`
-    }
-}
+  name: "amardeep",
+  salary: 50000,
+  designation: "senior developer",
+  details: function () {
+    return `Name is ${this.name}, Salary is ${this.salary}$ and Designation is ${this.designation}`;
+  },
+};
 
-let emps = Object.create(emp)
-emps.jobTitle = 'Software Developer';
+let emps = Object.create(emp);
+emps.jobTitle = "Software Developer";
 console.log(emp.details());
+
+//arr
+let arr = [1, 2, 3];
+for (let i = 0; i < arr.length; i++) {
+  console.log(i);
+}
+let arr2 = [1, 2, 3];
+arr2.forEach(function(element) {
+console.log(element);
+});
+//obj
+let obj = { a: 1, b: 2 };
+Object.keys(obj).forEach((key) => {
+  console.log(`${key}:${obj[key]}`);
+});
